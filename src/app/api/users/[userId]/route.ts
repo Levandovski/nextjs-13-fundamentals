ee02f42c92cf437d0a65d7ea8a0ef4a41860c3b1
Jesse Levandovski <jesse.levandovski@outlook.com>
2023-10-09 16:30:48 -0300

    Finalizando projeto

--- src/app/api/users/[userId]/route.ts
+++ src/app/api/users/[userId]/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
+console.log(process.env.TOKEN)
+console.log(process.env.SECREAT_KEY)
 export async function GET(request: NextRequest, { params }: any) {
   const { searchParams } = new URL(request.url);
   console.log(searchParams.get("name"));
